def4eb2e35864b58126eec3e73b2f6a9bca1256d
Henning Scheufler <henning.scheufler@web.de>
2024-04-28 15:36:54 +0200

    WIP: update benchmarksuite  TODO * reconstruction fails * surfaceTension benchmark fails

--- benchmarksuite/interfaceCapturing/advection/deformationSphereTet/deformationSphereTet/test_deformationSphereTet.py
+++ benchmarksuite/interfaceCapturing/advection/deformationSphereTet/deformationSphereTet/test_deformationSphereTet.py
@@ -8,5 +8,14 @@ def test_deformationSphereTet(run_reset_case):
     logs = oftest.path_logs()
     assert len(logs) > 0
     for log in logs:
-        assert oftest.case_status(log) == 'completed' # checks if run completes
+        print(f"current log file:", log)
+        if "gmshv493" in log:
+            lines =  oftest.tail_log(log)
+            foundKeyWord = False
+            for line in lines:
+                if "Done writing" in line:
+                    foundKeyWord = True
+            assert foundKeyWord
+        else:
+            assert oftest.case_status(log) == "completed"  # checks if run completes
 
