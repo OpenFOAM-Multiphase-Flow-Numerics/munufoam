353a00debbf460767ae95a44a3f3e64f5dff5f92
Henning Scheufler <henning.scheufler@web.de>
2024-04-28 13:58:14 +0200

    removed all files for documentation

--- benchmarksuite/interfaceCapturing/reconstruction/runAll.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd ${0%/*} || exit 1    # Run from this directory
-
-./circle/runAll.sh
-./circleTet/runAll.sh
-./sphere/runAll.sh
-./sphereTet/runAll.sh
--- buildDoc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd "${0%/*}" || exit                                # Run from this directory
-
-sphinx-build -b html ./doc ./docs/
